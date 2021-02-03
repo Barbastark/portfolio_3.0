d164952940082cc252f428c8854c185ede3a74ca
Barbastark <dorkinacage@gmail.com>
2021-02-03 10:45:09 +0100

    Sticky footer

--- src/js/pages/Contact.js
+++ src/js/pages/Contact.js
@@ -2,9 +2,10 @@ import React from "react"
 
 const Contact = () => {
     return(
-        <main>
-            <section className="about"> 
+        <main className="about">
+            <section className="about__main"> 
                 <h1>Contact</h1>
+                <p>Color contrast is a common web accessibility issue that is often overlooked. People who may have low vision, or may be colorblind, could encounter some difficulty distinguishing text from a background color if the contrast is insufficient.</p>
             </section>
         </main>
     )
